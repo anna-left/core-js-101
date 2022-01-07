8e86211e18c354cc764a0388598af18983f33b51
anna-left <anna_left@mail.ru>
2022-01-07 11:20:15 +0300

    fix: fix getAverage

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -52,13 +52,13 @@ function getCircleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-  if (value1 === Number.MAX_VALUE - 2 && value2 === Number.MAX_VALUE) {
-    return Number.MAX_VALUE - 1;
-  }
-  if (value1 === Number.MAX_VALUE && value2 === Number.MAX_VALUE / 2) {
-    return Number.MAX_VALUE / 4;
-  }
-  return (value1 + value2) / 2;
+  // if (value1 === Number.MAX_VALUE - 2 && value2 === Number.MAX_VALUE) {
+  //   return Number.MAX_VALUE - 1;
+  // }
+  // if (value1 === Number.MAX_VALUE && value2 === Number.MAX_VALUE / 2) {
+  //   return Number.MAX_VALUE / 4;
+  // }
+  return value1 / 2 + value2 / 2;
   // throw new Error('Not implemented');
 }
 
